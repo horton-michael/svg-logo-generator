f942825d8975b803a91968fa3814910b0830f1ab
mhorton <horton-michael@users.noreply.github.com>
2023-09-19 21:21:58 -0400

    adds Circle tests

--- __tests__/shapes.test.js
+++ __tests__/shapes.test.js
@@ -1,6 +1,4 @@
-const { Shape, Circle, Square } = require("../lib/shapes.js");
-// const { Circle } = require("../lib/shapes.js");
-// const { Square } = require("../lib/shapes.js");
+const { Shape } = require("../lib/shapes.js");
 
 describe("shapes", () => {
   // arrange
@@ -32,30 +30,9 @@ describe("circle", () => {
   it("should have a render method that returns a circle svg file", () => {
     expect(circleClass.render()).toEqual(
       `
-    <svg width="100" height="100">
-        <circle cx="50" cy="50" r="40" stroke="green" stroke-width="4" fill="${this.color}" />
-    </svg>`
-    );
-  });
-});
-
-describe("square", () => {
-  // arrange
-  const squareClass = new Square();
-  // act
-  // assert
-  it("should be an instance of the Shape class", () => {
-    expect(squareClass).toBeInstanceOf(Shape);
-  });
-  it("should have a render method", () => {
-    expect(squareClass).toHaveProperty("render");
-  });
-  it("should have a render method that returns a square svg file", () => {
-    expect(squareClass.render()).toEqual(
-      `
-    <svg width="100" height="100">
-        <rect width="100" height="100" fill="${this.color} />
-    </svg>`
+      <svg width="100" height="100">
+          <circle cx="50" cy="50" r="40" stroke="green" stroke-width="4" fill="${this.color}" />
+      </svg>`
     );
   });
 });
